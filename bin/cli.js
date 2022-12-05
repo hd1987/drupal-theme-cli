a65f23d4d7723fc0c6273c732ad4a9de84b759fd
Adi <huangdi1987@163.com>
2022-12-05 10:51:45 +0800

    Adjust create process.

--- bin/cli.js
+++ bin/cli.js
@@ -9,19 +9,16 @@ const name = 'drupal-theme-cli';
 program
   .name(name)
   .version(version)
-  .usage('<command> [option]');
+  .usage('[option]')
+  .description('Create a new project')
+  .option('-f, --force', 'overwrite target directory if it exists')
+  .action((options) => {
+    createModel(options);
+  })
 
 program.on('--help', () => {
   console.log();
-  console.log(`Run ${chalk.cyan(`${name} <command> --help`)} for detailed usage of given command.`);
+  console.log(`Run ${chalk.cyan(`${name} --help`)} for detailed usage of given command.`);
 });
-
-program
-  .command('create <project-name>')
-  .description('create a new project')
-  .option('-f, --force', 'overwrite target directory if it exists')
-  .action((name, options) => {
-    createModel(name, options);
-  })
 
 program.parse(process.argv);
